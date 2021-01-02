6474db4c8ce426baf59b804ac2528164cac8cdf4
Emad <emad@vt.edu>
2021-01-01 22:30:58 -0500

    some changes

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,7 @@
 using SimpleNavierStokes, Test
 
-@testset "foo" begin
-    x, y = 5, 7
-    @test foo(x, y) == 7
+@testset "Bare-bones test" begin
+    @test LidDrivenCavity(tfinal=2) == 2
     x = "blah"
-    @test_throws MethodError foo(x, y)
 end
 
-@testset "bar" begin
-    z = 4.
-    @test bar(z) == 1.
-end
